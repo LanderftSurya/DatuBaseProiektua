e5e6f2a685e192135ddc2d12d6a0955c7683598e
ComradeTxuria <sortega024@ikasle.ehu.eus>
2026-04-28 17:01:09 +0200

    liburutegiarekin arazoak konponduta

--- TestConexion.java
+++ TestConexion.java
@@ -1,20 +1,49 @@
 
 
+import java.sql.Statement;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class TestConexion {
     public Connection konexioaEzarri(String pUsuario, String pPassword) {
     	Connection con =null;
     	try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            String url = "jdbc:mysql://localhost:3306/Festibalak";
+            boolean badago;
+            String url = "jdbc:mysql://localhost:3306/";
             String usuario = pUsuario;
             String password = pPassword;
-
+            String datuBasea = "Festibalak";
             con = DriverManager.getConnection(url, usuario, password);
-
+            con.createStatement().executeUpdate("CREATE DATABASE IF NOT EXISTS " + datuBasea);
+            try(Statement stmt = con.createStatement()){
+            	stmt.executeQuery("SELECT * FROM Hiria LIMIT 1");
+            	badago = true;
+            }catch (SQLException e) {
+            	badago = false;
+            }
+            if(!badago) {
+            	File scriptFile = new File("copia_festibalak_amaituta.sql");
+                
+                try (Scanner s = new Scanner(scriptFile);
+                    Statement stmt = con.createStatement()) { 
+                    s.useDelimiter(";");    
+                    while (s.hasNext()) {
+                        String comando = s.next().trim();
+                        if (!comando.isEmpty()) {
+                            stmt.execute(comando);
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("Error: Ez dago datu basea deskargatuta.");
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
